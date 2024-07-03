633e02376fc4f9a23b881c9a40d99be337d2ecd0
Neto <j.altevirneto@gmail.com>
2024-07-03 08:43:26 -0300

    add funções de cálculo

---
+++ funcoes/calculos.js
@@ -0,0 +1,14 @@
+//declaração das funções de calculo
+
+let soma = (numA, numB) => {
+    numA + numB;
+}
+let subtracao = (numA, numB) => {
+    numA - numB;
+}
+function multiplicacao(numA, numB){
+    return numA * numB;    
+}
+function divisao(numA,numB){
+    return numA / numB;
+}
